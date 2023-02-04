55d275c6a2cdba88b6162157026bef39c6530ec9
XiantaoCheng <35281415+XiantaoCheng@users.noreply.github.com>
2023-02-04 14:52:04 -0600

    Update OrbitControls.js

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -225,7 +225,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		if ( state === STATE.NONE )
 		{
-			if ( event.button === 0 )
+			if ( event.button === 2 )
 				state = STATE.ROTATE;
 			if ( event.button === 1 )
 				state = STATE.ZOOM;
@@ -351,7 +351,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		switch ( event.keyCode ) {
 
-			/*case scope.keys.UP:
+			case scope.keys.UP:
 				scope.pan( new THREE.Vector3( 0, 1, 0 ) );
 				break;
 			case scope.keys.BOTTOM:
@@ -363,7 +363,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 			case scope.keys.RIGHT:
 				scope.pan( new THREE.Vector3( 1, 0, 0 ) );
 				break;
-			*/
+			
 			case scope.keys.ROTATE:
 				state = STATE.ROTATE;
 				break;
